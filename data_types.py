d50731ffb1af7f830563371d46fde8527449f84a
cinthiab23 <123271865+cinthiab23@users.noreply.github.com>
2024-09-05 23:13:27 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,15 @@
+# Literal
+x = 80
+print("Literal:", x)
+# Integer
+y = -60
+print("Integer:", y)
+# Float
+z = 14.5
+print("Float:", z)
+# Srings
+name = "Cinthia"
+print("String:", name)
+# Boolean
+is_active = True
+print("Boolean:", is_active)
