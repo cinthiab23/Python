4b1c8971e8c140d5d76f4c76ac69430b93c9fffc
cinthiab23 <123271865+cinthiab23@users.noreply.github.com>
2024-09-07 22:22:39 -0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,22 @@
+# kilograms to pounds
+
+# Conversion factor
+conversion_factor = 2.20462
+
+# Define 4 samples of kilograms
+sample_1 = 10
+sample_2 = 20
+sample_3 = 30
+sample_4 = 40
+
+# Conversion to pounds
+pounds_1 = sample_1 * conversion_factor
+pounds_2 = sample_2 * conversion_factor
+pounds_3 = sample_3 * conversion_factor
+pounds_4 = sample_4 * conversion_factor
+
+# output results
+print(f"{sample_1} kilograms is approximately equal to{pounds_1:.4f} pounds.")
+print(f"{sample_2} kilograms is approximately equal to {pounds_2:.4f} pounds.")
+print(f"{sample_3} kilograms is approximately equal to {pounds_3:.4f} pounds.")
+print(f"{sample_4} kilograms is approximately equal to {pounds_4:.4f} pounds.")
