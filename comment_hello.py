00d9cbdd85b1faf611be79c405e06011717a1a3b
cinthiab23 <123271865+cinthiab23@users.noreply.github.com>
2024-09-07 23:23:05 -0500

    Add files via upload

---
+++ comment_hello.py
@@ -0,0 +1,35 @@
+print("Hello World")
+print()
+
+
+# Comments assignment
+
+# Define the size of the tank in gallons
+
+tank_size = 15
+
+# Define the total distance traveled in miles
+
+distance = 600
+
+# Calculate miles per gallon (mpg) by dividing the distance by the tank size
+
+mpg = distance / tank_size
+
+
+print("Miles per gallon:", mpg)  # Output the result
+
+
+print()
+
+
+# Knock-Knock Joke program
+print("knock, knock")
+print("Who's there?")
+print("Yah")
+print("Yah who?")
+print("settle down, cowboy!")
+print()
+print("Knock, knock " "\n Who's there?")
+print("Nobel" "\n Nobel who?")
+print("No bell. " "\n That's why I knocked.")
