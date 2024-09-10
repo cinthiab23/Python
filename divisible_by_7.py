fb85b673f4a66a56f8627d234bc5a85c179db7fc
cinthiab23 <123271865+cinthiab23@users.noreply.github.com>
2024-09-09 23:29:43 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,6 @@
+# loop number 1-300
+for number in range(1, 300):
+    # Checking if number is divisible by 7
+    if number % 7 == 0:
+        # print the result divided by 7
+        print(number)
