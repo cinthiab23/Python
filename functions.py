2a7ee84385754492c07fa1ba761452f238c7ceb9
cinthiab23 <123271865+cinthiab23@users.noreply.github.com>
2024-09-23 18:49:22 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+def square(side):
+    area = side * side
+    print(" The area of the square is" + str(area) + "square units")
+
+
+def circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    print(
+        "The area of the circle is " + str(area) + "square units"
+    )  # What does the str
+
+
+square(8)
+circle(10)
